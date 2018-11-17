0baff19042577ee7e7a6cf98512c5ef1b8910349
Song Zhou <zhousong1993@gmail.com>
2018-11-17 14:12:55 +0800

    initial commit

---
+++ SZCoordinatorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SZCoordinatorDemo
+//
+//  Created by songzhou on 2018/11/17.
+//  Copyright © 2018 songzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
